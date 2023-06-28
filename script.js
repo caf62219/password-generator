67e8e84d097ae9fdc407586627191d657bb494b0
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-06-27 23:48:28 -0700

    changes to javascript getting closer to password but needs randomizing

--- script.js
+++ script.js
@@ -8,54 +8,105 @@ var generateBtn = document.querySelector("#generate");
   var symb= ["!","@","#","$","%","^","&","*","(",")","=","+","-","_"];
   var bank= "";
 
+function getUpper() {
+var confirmUpper=(Math.floor(Math.random()*upper.length));
+var randomUpper = upper[confirmUpper];
+return randomUpper;
+}
+
+function getLower(){
+var confirmLower=(Math.floor(Math.random()*lower.length));
+var randomLower = lower[confirmLower];
+return randomLower;
+}
+
+function getNumber() {
+var confirmNumber=(Math.floor(Math.random()*numb.length));
+var randomNumber = numb[confirmNumber];
+return randomNumber;
+}
+
+function getSymbol() {
+var confirmSymbol=(Math.floor(Math.random()*symb.length));
+var randomSymbol = symb[confirmSymbol];
+return randomSymbol;
+}
+
 function generatePassword () {
+bank= "";
+fullBank=""
 var length= window.prompt("How long do you want your password(8-128)?");
 var parseLength = parseInt(length);
 
 if (parseLength<8 || parseLength >128)  {
-  window.alert("Invalid must be 8-128 in length");
-  return - 1;   
+  return "Invalid must be 8-128 characters in length";   
 }
-let confirmUpper=(Math.floor(Math.random()*upper.length));
-let confirmLower=(Math.floor(Math.random()*lower.length));
-let confirmNumber=(Math.floor(Math.random()*numb.length));
-let confirmSymbol=(Math.floor(Math.random()*symb.length));
 
 
-var randomUpper = upper[confirmUpper];
-var randomLower = lower[confirmLower];
-var randomNumber = numb[confirmNumber];
-var randomSymbol = symb[confirmSymbol];
 
-
-console.log(randomUpper)
-console.log(randomLower)
-console.log(randomNumber)
-console.log(randomSymbol)
 
 var upperCase = window.confirm("Click ok if you want uppercase characters?");
 var lowerCase = window.confirm ("Click ok if you want lowercase characters?"); 
 var numbers = window.confirm ("Click ok if you want numbers?");
 var symbols = window.confirm("Click ok if you want symbols?");  
-return (parseLength, upperCase, lowerCase, numbers, symbols);
+//return (parseLength, upperCase, lowerCase, numbers, symbols);
 
 if (upperCase === true) {
-  bank = bank +_randomUpper
+  bank = bank + getUpper();
+  fullBank= fullBank.concat(upper);
 }
 
 if (lowerCase === true) {
-  bank = bank + randomLower;
+  bank = bank + getLower();
+  fullBank= fullBank.concat(lower);
 }
 
 if (numbers === true) {
-  bank = bank + randomNumber;
+  bank = bank + getNumber();
+fullBank= fullBank.concat(numb);
 }
 
 if (symbols === true) {
-  bank = bank + randomSymbol
+  bank = bank + getSymbol();
+fullBank= fullBank.concat(symb);
 }
 
+if (upperCase === false && lowerCase === false && numbers === false && symbols === false) {
+return "Need more character types"
 
+}
+console.log(fullBank)
+var startLength =bank.length;
+for (i = 0; i<parseLength-startLength; i++)
+{
+  var confirmBank=(Math.floor(Math.random()*fullBank.length));
+var randomChoice = fullBank[confirmBank];
+bank= bank + randomChoice;
+}
+console.log(bank)
+return bank
+//Help from Phind to randomize the password https://www.phind.com/search?cache=58a5884c-dea1-4e0b-ac6c-9d701c44b4d0
+
+/*function shuffleString(str) {
+  var array = str.split('');
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle
+  while (currentIndex !== 0) {
+      // Pick a remaining element
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // Swap it with the current element
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+
+  return array.join('');
+}
+return shuffleString(bank).toString()
+*/
 }
 
 // Write password to the #password input
@@ -73,6 +124,14 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+/*do I need something like this?
+function copyPassword() {
+
+  document.getElementById("display").select();
+
+  document.execCommand("Copy");
+
+  alert("Password copied to clipboard!"); */
 
 
 
