de34c0f04553ee540d6e346eeb443f0c36c9a4d9
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-06-29 18:06:30 -0700

    updates to Javascript options for symbols

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var symb = ["!","@","#","$","%","^","&","*","(",")","=","+","-","_",];
+var symb = ["!","@","#","$","%","^","&","*","(",")","=","+","-","_","'",",",".","/","?","{","[","]","}","|","~","`",";",":",">","<"];
 var bank = "";
 
 //function to get a random uppercase letter
